c078ee3881a5503c108a264e6e7e8ff1c3a2a818
Sarath Francis <sarathfrancis90@gmail.com>
2015-09-23 10:37:45 -0400

    just solved the neighbours issue

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -4,6 +4,7 @@ import com.sun.org.apache.xml.internal.security.algorithms.JCEMapper.Algorithm
 
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
+import scala.concurrent.duration.Duration
 
 /**
  * Created by sarathfrancis90 on 9/17/15.
@@ -13,15 +14,16 @@ object Project2 {
   sealed trait Rumor
   case class  MasterInit(noOfNodes:Int,topology:String,algorithm:String) extends Rumor
   case class  Gossip_NodeInit (neighbourlist:List[Int],noOfNodes:Int) extends Rumor
-  case class  Gossip() extends Rumor
-  case class  ReceivedGossip() extends Rumor
+  case object  Gossip extends Rumor
+  case object  ReceivedGossip extends Rumor
+  case class  EnoughGossips() extends  Rumor
 
   //variable to store no of nodes
  // var NoOfNodes:Int = _
   //list buffer to store nodes
-  val Nodes = new ListBuffer[ActorRef]()
+  var Nodes :ListBuffer[ActorRef] = new ListBuffer[ActorRef]()
   //variable to store Actor System
-  var System : ActorSystem = _
+  var MyActorSystem : ActorSystem = _
   //NeighbourList
   //val NeighbourList = new ListBuffer[ActorRef]()
 
@@ -29,91 +31,176 @@ object Project2 {
   def main (args: Array[String])
   {
 
-    var NoOfNodes:Int = args(0).toInt
+    val NoOfNodes: Int = args(0).toInt
     val Topology:String  = args(1)
     val Algorithm:String = args(2)
 
+
     //Creating Actor System
-    System = ActorSystem ("GossipSimulator")
+    MyActorSystem = ActorSystem ("GossipSimulator")
 
     //Creating MasterneighbourList.toList
-    val master = System.actorOf(Props(new Master), name ="Master")
+    val master = MyActorSystem.actorOf(Props(new Master), name ="Master")
 
     //Initiating Master
     master ! MasterInit(NoOfNodes,Topology,Algorithm)
+
+    MyActorSystem.awaitTermination()
 
   }
 
   class Node extends Actor with ActorLogging {
 
-    var myNeighbours :List[Int] = _
+    var myNeighboursListssss : ListBuffer[Int] = _
     var No_of_Nodes: Int =_
-    var Gossip_Count: Int =_
+    var Gossip_Count: Int = 0
+    var MasterRef :ActorRef = _
+    var neighbourRef:ActorRef = _
+
 
     def receive = {
+
       case Gossip_NodeInit(neighbourList,noOfNodes) =>
-        log.info("Node Initiated")
-        myNeighbours = neighbourList
+       //log.info("Node Initiated")
+        println("Node no. " + self.path.name + " initiated")
+        MasterRef = sender()
+        myNeighboursListssss = neighbourList.to[ListBuffer]
         No_of_Nodes = noOfNodes
+
+        printf("my neighbors are ")
+        myNeighboursListssss.foreach(printf("%d  ",_))
+        println(" ")
+
 
       case Gossip  =>
         Gossip_Count+=1
-        self ! ReceivedGossip
+        println("Gossip received at " + self.path.name)
+        printf("my neighbors are ")
+        myNeighboursListssss.foreach(printf("%d  ",_))
+        println(" ")
+        if(Gossip_Count == 10) {
+//          for(myNeighbour <-myNeighbours) {
+//            Nodes(myNeighbour) ! EnoughGossips
+//          }
+//          MasterRef ! EnoughGossips
+          log.info("I am done")
+        }
+        else {
+          Thread.sleep(1000)
+          self ! ReceivedGossip
+        }
+
 
       case ReceivedGossip =>
-        if(Gossip_Count == 10) {
-          
-        }
-        if(!myNeighbours.isEmpty){
-          Nodes(myNeighbours(Random.nextInt(myNeighbours.length))) !Gossip
-        }
+        println("ReceivedGossip received at " + self.path.name)
+        printf("my neighbors are ")
+        myNeighboursListssss.foreach(printf("%d  ",_))
+        Thread.sleep(1000)
 
+        if(myNeighboursListssss.length != 0){
+          var aRandomNumber = Random.nextInt(myNeighboursListssss.size)
+          var randomNeighbour: Int = myNeighboursListssss(aRandomNumber)
+
+          if(self.path.name == Nodes(randomNeighbour).path.name) {
+            printf ("i am %s\n", self.path.name)
+            printf("my neighbors are ")
+            myNeighboursListssss.foreach(printf("%d  ",_))
+            println("\nsending to self")
+            printf("i generated random number %d\n", aRandomNumber)
+            printf("i am going to send to %d\n", randomNeighbour)
+            printf("that neighbors name is %s\n", Nodes(randomNeighbour).path.name)
+            Thread.sleep(3000)
+          }
+          Nodes(randomNeighbour) !  Gossip
+        }
+        else
+          println("hi")
+
+      case EnoughGossips  =>
+           log.info("Received EnoughGossips")
+         if(myNeighboursListssss.contains(sender().path.name.toInt))  {
+         // myNeighbours -= sender().path.name.toInt
+         }
     }
   }
 
   class Master extends Actor with  ActorLogging {
 
-    var neighbours = new ListBuffer[Int]
-    var neighbourList :List[Int] =_
+    val neighbours:ListBuffer[Int] = new ListBuffer[Int]
+    var noOfcompletedNodes : Int = 0
+    var timeBeforeStartGossip: Long = _
+    var timeAfterGossip:Long =_
+    var no_Of_Nodes:Int = _
+    var networktopology: String = _
+    var currentalgorithm: String =_
 
     def receive = {
 
       //Initiating Master by the Main Process
       case MasterInit(noOfNodes, topology, algorithm) =>
 
-        for (i <- 0 until noOfNodes - 1) {
-          Nodes += System.actorOf(Props(new Node), name = "Node"+(i + 1).toString)
+        no_Of_Nodes = noOfNodes
+        networktopology = topology
+        currentalgorithm = algorithm
+
+        for (i <- 0 until no_Of_Nodes) {
+          Nodes += MyActorSystem.actorOf(Props(new Node), name = s"worker$i")
 
         }
-        if (topology == "full") {
-          for (i <- 0 until noOfNodes - 1) {
-            for (j <- 0 until noOfNodes - 1) {
-              if (j != i) {
-                neighbours += j
-              }
-            }
-            neighbourList = neighbours.toList
+        //println(no_Of_Nodes +" " +networktopology +" "+ currentalgorithm)
+
+        if (networktopology == "full") {
+
+          for (i <- 0 until no_Of_Nodes) {
             neighbours.clear()
-            Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
+
+            for (j <- 0 until no_Of_Nodes) {
+              if (j != i)
+                neighbours += j
+            }
+
+//            printf ("sending int to %s with neighbors ", Nodes(i).path.name)
+//            neighbours.foreach(printf("--%d ", _))
+//            printf("\n")
+            Thread.sleep(2000)
+            Nodes(i) ! Gossip_NodeInit(neighbours.toList, no_Of_Nodes)
           }
+          //println(Nodes.size)
         }
-        if (topology =="line") {
+        else if (networktopology =="line") {
 
           //Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
 
 
         }
-        if(topology == "3D")  {
+        else if(networktopology == "3D")  {
 
           //Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
 
         }
-        if(topology == "imp3D") {
+         else if(networktopology == "imp3D") {
 
           //Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
 
         }
-        Nodes(Random.nextInt(noOfNodes)) ! Gossip
+
+        Nodes(Random.nextInt(Nodes.size)) ! Gossip
+        timeBeforeStartGossip =  System.currentTimeMillis()
+
+
+      case EnoughGossips  =>
+          log.info(sender().path.name.toString +" has completed")
+        noOfcompletedNodes += 1
+        if(noOfcompletedNodes == Nodes.length)  {
+
+        timeAfterGossip = System.currentTimeMillis()
+          println("Time taken to converge :" + (timeAfterGossip-timeBeforeStartGossip) )
+
+          context.system.shutdown()
+        }
+
+
     }
+
   }
 }
