5e9c15bde13a6c561e51de9d8072852529e96f7c
Sarath Francis <sarathfrancis90@gmail.com>
2015-09-26 07:29:35 -0400

    in the middle of pushsum

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -13,8 +13,10 @@ object Project2 {
 
   sealed trait Rumor
   case class  MasterInit(noOfNodes:Int,topology:String,algorithm:String) extends Rumor
-  case class  Gossip_NodeInit (neighbourlist:List[Int],noOfNodes:Int) extends Rumor
+  case class  Gossip_NodeInit (neighbourlist:List[Int]) extends Rumor
+  case class  PushSum_NodeInit  (neighbourList:List[Int]) extends Rumor
   case object  Gossip extends Rumor
+  case object  PushSum extends  Rumor
   case object  ReceivedGossip extends Rumor
   case class  EnoughGossips() extends  Rumor
 
@@ -51,76 +53,94 @@ object Project2 {
 
   class Node extends Actor with ActorLogging {
 
-    var myNeighboursListssss : ListBuffer[Int] = _
-    var No_of_Nodes: Int =_
+    var myNeighbours : ListBuffer[Int] = _
+   // var No_of_Nodes: Int =_
     var Gossip_Count: Int = 0
     var MasterRef :ActorRef = _
     var neighbourRef:ActorRef = _
+    var s: Double = _
+    var w: Double = _
 
 
     def receive = {
 
-      case Gossip_NodeInit(neighbourList,noOfNodes) =>
+      case Gossip_NodeInit(neighbourList) =>
        //log.info("Node Initiated")
         println("Node no. " + self.path.name + " initiated")
         MasterRef = sender()
-        myNeighboursListssss = neighbourList.to[ListBuffer]
-        No_of_Nodes = noOfNodes
+        myNeighbours = neighbourList.to[ListBuffer]
+       // No_of_Nodes = noOfNodes
 
-        printf("my neighbors are ")
-        myNeighboursListssss.foreach(printf("%d  ",_))
-        println(" ")
+//        printf("my neighbors are ")
+//        myNeighbours.foreach(printf("%d  ",_))
+//        println(" ")
 
+      case PushSum_NodeInit(neighbourList)  =>
+
+        println("Node no. " + self.path.name + " initiated")
+        myNeighbours = neighbourList.to[ListBuffer]
+        MasterRef =sender()
+        s = self.path.name.toDouble
+        w = 1
+        println("S and W of Node no. " + self.path.name + " is " + s +" and "+ w)
 
       case Gossip  =>
+//        printf("my neighbors are ")
+//        myNeighbours.foreach(printf("%d  ",_))
+//        println(" ")
         Gossip_Count+=1
-        println("Gossip received at " + self.path.name)
-        printf("my neighbors are ")
-        myNeighboursListssss.foreach(printf("%d  ",_))
-        println(" ")
-        if(Gossip_Count == 10) {
-//          for(myNeighbour <-myNeighbours) {
-//            Nodes(myNeighbour) ! EnoughGossips
-//          }
-//          MasterRef ! EnoughGossips
-          log.info("I am done")
-        }
-        else {
-          Thread.sleep(1000)
-          self ! ReceivedGossip
-        }
+        if(Gossip_Count <=10)
+        println("Gossip number "+ Gossip_Count + " received at " + self.path.name + " from "+ sender().path.name)
 
+        if(Gossip_Count == 10) {
+//          println("Gossip number "+ Gossip_Count + " received at " + self.path.name + " from "+ sender().path.name)
+          for(myNeighbour <-myNeighbours) {
+            Nodes(myNeighbour) ! EnoughGossips
+          }
+          MasterRef ! EnoughGossips
+          println("I am done - Node "+ self.path.name)
+        }
+//         Thread.sleep(1000)
+          self ! ReceivedGossip
 
       case ReceivedGossip =>
-        println("ReceivedGossip received at " + self.path.name)
-        printf("my neighbors are ")
-        myNeighboursListssss.foreach(printf("%d  ",_))
-        Thread.sleep(1000)
+//        println("ReceivedGossip received at " + self.path.name)
+//        printf("my neighbors are ")
+//        myNeighbours.foreach(printf("%d  ",_))
+//        println()
+//        Thread.sleep(1000)
 
-        if(myNeighboursListssss.length != 0){
-          var aRandomNumber = Random.nextInt(myNeighboursListssss.size)
-          var randomNeighbour: Int = myNeighboursListssss(aRandomNumber)
-
-          if(self.path.name == Nodes(randomNeighbour).path.name) {
-            printf ("i am %s\n", self.path.name)
-            printf("my neighbors are ")
-            myNeighboursListssss.foreach(printf("%d  ",_))
-            println("\nsending to self")
-            printf("i generated random number %d\n", aRandomNumber)
-            printf("i am going to send to %d\n", randomNeighbour)
-            printf("that neighbors name is %s\n", Nodes(randomNeighbour).path.name)
-            Thread.sleep(3000)
-          }
+        if(myNeighbours.length > 0) {
+          val aRandomNumber = Random.nextInt(myNeighbours.size)
+          val randomNeighbour: Int = myNeighbours(aRandomNumber)
           Nodes(randomNeighbour) !  Gossip
         }
-        else
-          println("hi")
+//          if(self.path.name == Nodes(randomNeighbour).path.name) {
+//            printf ("i am %s\n", self.path.name)
+//            printf("my neighbors are ")
+//            myNeighbours.foreach(printf("%d  ",_))
+//            println("\nsending to self")
+//            printf("i generated random number %d\n", aRandomNumber)
+//            printf("i am going to send to %d\n", randomNeighbour)
+//            printf("that neighbors name is %s\n", Nodes(randomNeighbour).path.name)
+//            Thread.sleep(3000)
+//          }
+        self !ReceivedGossip
 
       case EnoughGossips  =>
-           log.info("Received EnoughGossips")
-         if(myNeighboursListssss.contains(sender().path.name.toInt))  {
-         // myNeighbours -= sender().path.name.toInt
+         if(myNeighbours.contains(sender().path.name.toInt))  {
+           println("Node "+ self.path.name +" Received EnoughGossips from " + sender().path.name)
+         myNeighbours -= sender().path.name.toInt
+          // if(myNeighbours.length != 0) {
+//               printf("my neighbors are ")
+//               myNeighbours.foreach(printf("%d  ",_))
+//               println()
+//               Thread.sleep(3000)
+          // }
+
          }
+        //Thread.sleep(1000)
+         self ! ReceivedGossip
     }
   }
 
@@ -144,7 +164,7 @@ object Project2 {
         currentalgorithm = algorithm
 
         for (i <- 0 until no_Of_Nodes) {
-          Nodes += MyActorSystem.actorOf(Props(new Node), name = s"worker$i")
+          Nodes += MyActorSystem.actorOf(Props(new Node), name = i.toString)
 
         }
         //println(no_Of_Nodes +" " +networktopology +" "+ currentalgorithm)
@@ -162,34 +182,101 @@ object Project2 {
 //            printf ("sending int to %s with neighbors ", Nodes(i).path.name)
 //            neighbours.foreach(printf("--%d ", _))
 //            printf("\n")
-            Thread.sleep(2000)
-            Nodes(i) ! Gossip_NodeInit(neighbours.toList, no_Of_Nodes)
+            if(currentalgorithm == "gossip")  {
+
+              Nodes(i) ! Gossip_NodeInit(neighbours.toList)
+
+            }
+            else if( currentalgorithm == "pushsum") {
+
+              Nodes(i)  ! PushSum_NodeInit(neighbours.toList)
+
+            }
+
+
+            //Thread.sleep(2000)
           }
+
           //println(Nodes.size)
         }
         else if (networktopology =="line") {
 
-          //Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
+          for(i <- 0 until no_Of_Nodes) {
+            neighbours.clear()
+            if(i==0) neighbours += i+1
+            else if(i== (no_Of_Nodes-1)) neighbours += i-1
+            else {
+              neighbours += i-1
+              neighbours += i+1
+            }
 
+            Nodes(i) ! Gossip_NodeInit(neighbours.toList)
+            //Thread.sleep(2000)
+          }
+          if(currentalgorithm == "gossip")  {
+
+          }
+          else if( currentalgorithm == "pushsum") {
+
+          }
 
         }
         else if(networktopology == "3D")  {
-
+//          if((Math.cbrt(no_Of_Nodes).toInt) == Math.cbrt(no_Of_Nodes))
+          val cube_side: Double = Math.cbrt(no_Of_Nodes)
           //Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
+          for( i <- 0 until no_Of_Nodes)  {
+            for (j <- 0 until no_Of_Nodes)  {
+              for (k <- 0 until no_Of_Nodes)  {
+
+
+              }
+            }
+          }
+
+
+          if(currentalgorithm == "gossip")  {
+
+          }
+          else if( currentalgorithm == "pushsum") {
+
+          }
 
         }
          else if(networktopology == "imp3D") {
 
           //Nodes(i) ! Gossip_NodeInit(neighbourList, noOfNodes)
 
+
+
+          if(currentalgorithm == "gossip")  {
+
+          }
+          else if( currentalgorithm == "pushsum") {
+
+          }
+
         }
 
-        Nodes(Random.nextInt(Nodes.size)) ! Gossip
+
+        if(currentalgorithm == "gossip")  {
+
+          // Nodes(Random.nextInt(Nodes.size)) ! Gossip
+
+        }
+        else if( currentalgorithm == "pushsum") {
+
+          Nodes(Random.nextInt(Nodes.size)) ! PushSum
+
+        }
+
+
+
         timeBeforeStartGossip =  System.currentTimeMillis()
 
 
       case EnoughGossips  =>
-          log.info(sender().path.name.toString +" has completed")
+          println("Node "+sender().path.name+" has completed")
         noOfcompletedNodes += 1
         if(noOfcompletedNodes == Nodes.length)  {
 
